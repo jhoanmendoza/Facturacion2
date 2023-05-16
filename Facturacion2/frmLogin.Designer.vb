88b8f7a1affac1d7579047b91de7e3084fc9e6c5
jhoan mendoza <jhoan38flo@gmail.com>
2023-05-16 15:19:46 -0500

    Agregar archivos de proyecto.

---
+++ Facturacion2/frmLogin.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblUsuario = New System.Windows.Forms.Label()
+        Me.lblClave = New System.Windows.Forms.Label()
+        Me.txtUsuario = New System.Windows.Forms.TextBox()
+        Me.txtClave = New System.Windows.Forms.TextBox()
+        Me.btnAceptar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblUsuario
+        '
+        Me.lblUsuario.AutoSize = True
+        Me.lblUsuario.Location = New System.Drawing.Point(49, 54)
+        Me.lblUsuario.Name = "lblUsuario"
+        Me.lblUsuario.Size = New System.Drawing.Size(46, 13)
+        Me.lblUsuario.TabIndex = 0
+        Me.lblUsuario.Text = "Usuario:"
+        '
+        'lblClave
+        '
+        Me.lblClave.AutoSize = True
+        Me.lblClave.Location = New System.Drawing.Point(49, 102)
+        Me.lblClave.Name = "lblClave"
+        Me.lblClave.Size = New System.Drawing.Size(37, 13)
+        Me.lblClave.TabIndex = 1
+        Me.lblClave.Text = "Clave:"
+        '
+        'txtUsuario
+        '
+        Me.txtUsuario.Location = New System.Drawing.Point(131, 47)
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(163, 20)
+        Me.txtUsuario.TabIndex = 2
+        '
+        'txtClave
+        '
+        Me.txtClave.Location = New System.Drawing.Point(131, 99)
+        Me.txtClave.Name = "txtClave"
+        Me.txtClave.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtClave.Size = New System.Drawing.Size(163, 20)
+        Me.txtClave.TabIndex = 3
+        '
+        'btnAceptar
+        '
+        Me.btnAceptar.Location = New System.Drawing.Point(73, 160)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAceptar.TabIndex = 4
+        Me.btnAceptar.Text = "&Aceptar"
+        Me.btnAceptar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(214, 160)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancelar.TabIndex = 5
+        Me.btnCancelar.Text = "&Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(412, 250)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnAceptar)
+        Me.Controls.Add(Me.txtClave)
+        Me.Controls.Add(Me.txtUsuario)
+        Me.Controls.Add(Me.lblClave)
+        Me.Controls.Add(Me.lblUsuario)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Ingreso al Sistema"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblUsuario As Label
+    Friend WithEvents lblClave As Label
+    Friend WithEvents txtUsuario As TextBox
+    Friend WithEvents txtClave As TextBox
+    Friend WithEvents btnAceptar As Button
+    Friend WithEvents btnCancelar As Button
+End Class
---
+++ Facturacion2/frmLogin.vb
@@ -0,0 +1,55 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
+
+        If txtUsuario.Text = "" Then
+            MsgBox("Debe ingresar un usuario", MsgBoxStyle.Information, "Error")
+            txtUsuario.Focus()
+            Exit Sub
+        End If
+
+        If txtClave.Text = "" Then
+            MsgBox("Debe ingresar una clave de usuario", MsgBoxStyle.Information, "Error")
+            txtClave.Focus()
+            Exit Sub
+        End If
+        'ABRIMOS CONEXION CON LA BASE DE DATOS
+        Dim miCadena As String
+        Dim miSQL As String
+        Dim miConexion As OleDbConnection
+        Dim miAdaptador As OleDbDataAdapter
+        Dim miDataSet As DataSet
+
+        'ESTABLECE CONEXION CON LA BASE DE DATOS
+        miCadena = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Productos.accdb"
+        miConexion = New OleDbConnection(miCadena)
+
+        'HACE LA CONSULTA A LA BASE DE DATOS
+        miAdaptador = New OleDbDataAdapter
+        miSQL = "select (1) FROM tblUsuario WHERE id = '" +
+            txtUsuario.Text + "' and  clave ='" + txtClave.Text + "'"
+        miAdaptador.SelectCommand = New OleDbCommand(miSQL, miConexion)
+        miDataSet = New DataSet
+        miDataSet.Tables.Add("usuario")
+        miAdaptador.Fill(miDataSet.Tables("usuario"))
+
+        If miDataSet.Tables("usuario").Rows.Count = 0 Then
+            MsgBox("usuarios o clave no validos", MsgBoxStyle.Information, "ERROR")
+            txtUsuario.Text = ""
+            txtClave.Text = ""
+            txtUsuario.Focus()
+            miConexion.Close()
+            Exit Sub
+        End If
+
+        MsgBox("fuck yea", MsgBoxStyle.Exclamation, "como lovio?")
+
+
+
+    End Sub
+End Class
